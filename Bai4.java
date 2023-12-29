7f47a8bab5383f77d701c5fbd8f053f812ff8c5e
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-29 21:47:11 +0700

    Update Bai4.java

--- Bai4.java
+++ Bai4.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
-class Bai4 {
-	public static void main (String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Nhap so can tinh: ");
-	int a = sc.nextInt();
-	double sum=0;
-	int i=1;
-	while (i<=a)
-	{sum = sum + 1.0/(i*2); i++;}
-	System.out.println("Tong từ 1 đến so n là: "+sum);
-	}
+public class Bai4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        int i = 1;
+        System.out.print("Nhap so can tinh: ");
+        int a = sc.nextInt();
+        if (a < 1) {
+            System.out.println("\nSo phai lon hon hoac bang 1. Nhap lai !");
+        } else {
+            while (i <= a) {
+                sum = sum + 1.0/(i*2);
+				i++;
+            }
+            System.out.println("Tong tu 1 den so n la: " + sum);
+        }
+    }
 }
