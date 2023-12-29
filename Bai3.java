f8a498eb6510f9c02f9ec9d28ddc277277df7416
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-29 21:23:34 +0700

    Add files via upload

---
+++ Bai3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Bai3 {
+	public static void main (String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Nhap so can tinh: ");
+	int a = sc.nextInt();
+	double sum=0;
+	int i=1;
+	while (i<=a)
+	{sum = sum + 1.0/i; i++;}
+	System.out.println("Tong từ 1 đến so n là: "+sum);
+	}
+}
