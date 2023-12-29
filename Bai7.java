e97744313ad829106b813a45e0902d3bc6459930
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-29 22:01:15 +0700

    Add files via upload

---
+++ Bai7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bai7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double sum = 0;
+        int i = 1;
+        System.out.print("Nhap so can tinh: ");
+        int a = sc.nextInt();
+        if (a < 1) {
+            System.out.println("\nSo phai lon hon hoac bang 1. Nhap lai !");
+        } else {
+            while (i <= a) {
+                sum = sum + (double)i/(i+1);
+				i++;
+            }
+            System.out.println("Ket qua la: " + sum);
+        }
+    }
+}
