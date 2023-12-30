1f37da1d52b46c44b830066e681f9edfd004878f
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-30 21:17:35 +0700

    Fix bug

--- Bai9.java
+++ Bai9.java
@@ -7,14 +7,10 @@ public class Bai9 {
         int i = 1;
         System.out.print("Nhap so can tinh: ");
         int a = sc.nextInt();
-        if (a < 1) {
-            System.out.println("\nSo phai lon hon hoac bang 1. Nhap lai !");
-        } else {
-            while (i <= a) {
-                kq = kq * i;
-				i++;
-            }
-            System.out.println("Ket qua la: " + kq);
-        }
+        while (i <= a) {
+        	kq = kq * I;
+		i++;
+	}
+        System.out.println("Ket qua la: " + kq);
     }
 }
