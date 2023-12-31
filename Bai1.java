fbffb8d68b22c73f0d22ecf5323c03fe56122de0
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-31 21:53:47 +0700

    Update Bai1.java

--- Bai1.java
+++ Bai1.java
@@ -4,10 +4,10 @@ class Bai1 {
 	public static void main (String[] args) {
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Nhap so can tinh: ");
-	int a = sc.nextInt();
+	int n = sc.nextInt();
 	int sum=0;
 	int i=0;
-	while (i<=a)
+	while (i<=n)
 	{sum = sum + i; i++;}
 	System.out.println("Tong từ 1 đến so n là: "+sum);
 	}
