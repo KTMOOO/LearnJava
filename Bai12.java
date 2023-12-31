870be86dca373682695fed4f55e908ef2e2c1118
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2023-12-31 22:21:12 +0700

    Update Bai12.java

---
+++ Bai12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bai12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long pt = 1;
+        long kq = 0;
+        int i = 1;
+        System.out.print("Nhap so can tinh: ");
+        int x = sc.nextInt();
+        System.out.print("Nhap so mu: ");
+        int n = sc.nextInt();
+        while (i <= n) {
+        	pt *= x;
+            kq += pt; 
+            i++;
+	    }
+        System.out.println("Ket qua la: " + kq);
+    }
+}
