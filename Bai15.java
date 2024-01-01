67664fcf3bdd1ca187a77f22910bb2c8c1229f10
TMK29 <121551019+KTMOOO@users.noreply.github.com>
2024-01-01 21:54:01 +0700

    Add files via upload

---
+++ Bai15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bai15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long ms = 0;
+        double kq = 0;
+        int i = 0;
+        System.out.print("Nhap so can tinh: ");
+        int n = sc.nextInt();
+        while (i <= n) {
+        	ms += x;
+            kq += 1/x; 
+            i++;
+	    }
+        System.out.println("Ket qua la: " + kq);
+    }
+}
